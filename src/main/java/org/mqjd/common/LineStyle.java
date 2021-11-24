5094f7c115497727c3f7049688ace6ad0420a3ef
mqjd <1378415278@qq.com>
2021-11-24 17:48:56 +0800

    init

--- src/main/java/org/mqjd/common/LineStyle.java
+++ src/main/java/org/mqjd/common/LineStyle.java
@@ -1,7 +1,7 @@
 package org.mqjd.common;
 
 public enum LineStyle {
-    TOP_SINGLE(SpecialCharacter.TOP_SINGLE_LINE),
+    TOP_SINGLE(SpecialCharacter.TOP_SINGLE_LINE_HORIZONTAL),
     MIDDLE_SINGLE(SpecialCharacter.MIDDLE_SINGLE_LINE),
     BOTTOM_SINGLE(SpecialCharacter.BOTTOM_SINGLE_LINE);
     private final String text;
--- src/main/java/org/mqjd/common/SpecialCharacter.java
+++ src/main/java/org/mqjd/common/SpecialCharacter.java
@@ -1,7 +1,28 @@
 package org.mqjd.common;
 
 public class SpecialCharacter {
-    public static final String TOP_SINGLE_LINE = "¯";
+    public static final String TOP_SINGLE_LINE_HORIZONTAL = "¯";
     public static final String MIDDLE_SINGLE_LINE = "—";
+    public static final String MIDDLE_DOUBLE_LINE = "═";
+    public static final String MIDDLE_DOUBLE_LINE_VERTICAL = "║";
     public static final String BOTTOM_SINGLE_LINE = "_";
+    public static final String CHARACTER1 = "┌";
+    public static final String CHARACTER2 = "└";
+    public static final String CHARACTER3 = "┘";
+    public static final String CHARACTER4 = "┐";
+    public static final String CHARACTER5 = "┬";
+    public static final String CHARACTER6 = "├";
+    public static final String CHARACTER7 = "┴";
+    public static final String CHARACTER8 = "┤";
+    public static final String CHARACTER9 = "┼";
+
+    public static final String CHARACTER11 = "╔";
+    public static final String CHARACTER12 = "╚";
+    public static final String CHARACTER13 = "╝";
+    public static final String CHARACTER14 = "╗";
+    public static final String CHARACTER15 = "╦";
+    public static final String CHARACTER16 = "╠";
+    public static final String CHARACTER17 = "╩";
+    public static final String CHARACTER18 = "╣";
+    public static final String CHARACTER19 = "╬";
 }
